6cbbd2e0424f246291834f384ea9891abbb1881d
Samantha Lastig <slastig13@gmail.com>
2020-01-25 14:37:28 -0500

    teaching slice 4

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,44 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const Test = require('../db/models/test')
+const Student = require('../db/models/student')
 
+router.get('/', async (req, res, next) => {
+  try {
+    let tests = await Test.findAll()
+    res.send(tests)
+  } catch (error) {
+    next(error)
+  }
+})
 
-module.exports = router;
+router.get('/:id', async (req, res, next) => {
+  try {
+    let test = await Test.findById(req.params.id)
+    res.send(test)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/student/:studentId', async (req, res, next) => {
+  try {
+    let student = await Student.findById(req.params.studentId)
+    let test = await Test.create(req.body)
+    let studentTest = await test.setStudent(student)
+    res.status(201).send(studentTest)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    let test = await Test.findById(req.params.id)
+    await test.destroy()
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router

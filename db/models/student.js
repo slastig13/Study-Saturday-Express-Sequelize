6e422f52ac954f4723de1e7105932706c59d816a
Samantha Lastig <slastig13@gmail.com>
2020-01-25 11:28:02 -0500

    first section done

--- db/models/student.js
+++ db/models/student.js
@@ -1,8 +1,34 @@
-'use strict';
+'use strict'
 
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
 
-module.exports = Student;
+Student.beforeCreate(student => {
+  let firstName =
+    student.firstName[0].toUpperCase() + student.firstName.slice(1)
+
+  let lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
+
+  student.firstName = firstName
+  student.lastName = lastName
+})
+
+module.exports = Student

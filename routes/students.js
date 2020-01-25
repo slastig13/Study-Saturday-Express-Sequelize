8d250ea3137a396147ee4a47d27882c4c7605d22
Samantha Lastig <slastig13@gmail.com>
2020-01-25 14:23:55 -0500

    cycle 3

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,74 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const Student = require('../db/models/student')
 
+router.get('/', async (req, res, next) => {
+  try {
+    const students = await Student.findAll()
+    res.send(students)
+  } catch (error) {
+    next(error)
+  }
+})
 
-module.exports = router;
+router.get('/:id', async (req, res, next) => {
+  try {
+    let id = req.params.id
+
+    let student = await Student.findOne({
+      where: {
+        id: id
+      }
+    })
+    if (student) res.send(student)
+    else res.sendStatus(404)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    let newStudent = await Student.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email
+    })
+
+    res.status(201).send(newStudent)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    let studentToUpdate = await Student.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    studentToUpdate = await studentToUpdate.update({
+      firstName: req.body.firstName
+    })
+    res.send(studentToUpdate)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    let student = await Student.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    await student.destroy()
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,14 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const Students = require('../db/models/student')
 
+// router.get('/students', (req, res, next) => {
+//   try {
+//     let students = Students.findAll()
+//     console'log
+//     res.json(students)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-module.exports = router;
+module.exports = router
